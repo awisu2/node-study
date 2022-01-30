041c56839c1c5d7b75acdc566b43859188703cc0
awisu2 <awisu2@gmail.com>
2022-01-30 11:59:38 +0900

    childprocessにtasklistの取得処理を追加

--- childprocess/test/index.js
+++ childprocess/test/index.js
@@ -1,8 +1,16 @@
 var assert = require('assert')
-var { spawnLs } = require('../index')
+var { spawnLs, taskList } = require('../index')
 
 describe('index.js', function () {
   it('spawn ls or dir', function () {
     assert.ok(spawnLs() != '')
+  })
+
+  describe('tasklist', function () {
+    this.timeout(5000)
+
+    it('return any value', function () {
+      assert.ok(taskList() != '')
+    })
   })
 })
--- mocha/test/test.js
+++ mocha/test/test.js
@@ -3,6 +3,9 @@ var assert = require('assert')
 // describe: テスト概要。出力にグループ分けするテキストとindentを付けてくれる(実際のテストはしない)
 describe('Array', function () {
   describe('#indexOf()', function () {
+    // 特定のテストのtimeout時間を変更
+    this.timeout(5000)
+
     // it: テスト。第一引数はテスト詳細
     it('should return -1 when the value is not present', function () {
       assert.equal([1, 2, 3].indexOf(4), -1)
